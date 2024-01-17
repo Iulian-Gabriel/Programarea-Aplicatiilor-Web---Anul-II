249af8f329285a7e66555bdbec55f4d8718aa587
Iulian-Gabriel <cheoseaua.m.iuliagnabriel22@stud.rau.ro>
2024-01-17 19:22:23 +0200

    homework 2.3 added but currently bugged

---
+++ Homework 2/Homework 2.3/2.3.js
@@ -0,0 +1,69 @@
+    const item_container = document.getElementById("items");
+    const item_template = document.getElementById("itemTemplate");
+    const button = document.getElementById("add");
+
+    let items = getItems();
+    function getItems(){
+    const value = localStorage.getItem("todo") || "[]";
+
+        return JSON.parse(value);
+    }
+
+    function setItems(items){
+        const itemsJson = JSON.stringify(items);
+        localStorage.setItem("todo", itemsJson);
+    }
+
+    function addItem(){
+        items.unshift({
+            description: "",
+            completed: false
+        });
+        setItems(items)
+        refreshList()
+    }
+
+    function updateItem(item, key, value){
+        item[key] = value;
+        setItems(items);    
+        refreshList();
+    }
+
+    function refreshList(){
+        items.sort((a, b) =>{
+            if(a.completed){
+                return 1;
+            }
+            if(b.completed){
+                return -1;
+            }
+            return a.description < b.description ? -1 : 1;
+        });
+        item_container.innerHTML = "";
+    
+        for (const item of items){
+            const itemElement = item_template.cloneNode(true);
+            const descriptionInput = itemElement.querySelector(".item-description");
+            const completedInput = itemElement.querySelector(".item-completed");
+    
+            descriptionInput.value = item.description;
+            completedInput.checked = item.completed;
+
+            descriptionInput.addEventListener("change", () =>{
+                updateItem(item, "description", descriptionInput.value);
+            });
+
+            completedInput.addEventListener("change", () =>{
+                updateItem(item, "completed", completedInput.checked);
+            });
+
+            item_container.append(itemElement);
+        }
+    }
+    
+    button.addEventListener("click", () =>{
+        addItem();
+
+    })
+    refreshList();
+
